b7af5f255375f678773b44777964107e57da645c
RamiroAHG <152554908+RamiroAHG@users.noreply.github.com>
2025-09-26 13:41:36 -0400

    this new one is ramiro project

---
+++ ramiro/ramiro/ramiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ramiroApp.swift
+//  ramiro
+//
+//  Created by Ramiro Herdocia on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ramiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

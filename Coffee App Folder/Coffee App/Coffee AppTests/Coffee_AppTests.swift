bacb54a31662e3cd8e41e585486434c51fbf9f89
RamiroAHG <152554908+RamiroAHG@users.noreply.github.com>
2025-11-30 17:24:49 -0500

    Nov 30 2025

---
+++ Coffee App Folder/Coffee App/Coffee AppTests/Coffee_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_AppTests.swift
+//  Coffee AppTests
+//
+//  Created by Ramiro Herdocia on 11/29/25.
+//
+
+import Testing
+@testable import Coffee_App
+
+struct Coffee_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Coffee App Folder/Coffee App/Coffee AppUITests/Coffee_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Coffee_AppUITestsLaunchTests.swift
+//  Coffee AppUITests
+//
+//  Created by Ramiro Herdocia on 11/29/25.
+//
+
+import XCTest
+
+final class Coffee_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
